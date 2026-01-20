b52b890e969b7e30408a839b113c02d878faddd0
0xshreygajjar <shreygajjar786@gmail.com>
2026-01-20 15:52:51 +0530

    Added| privy configuration with orderly network

--- src/components/orderlyProvider/index.tsx
+++ src/components/orderlyProvider/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { FC, ReactNode, useEffect } from "react";
+import { WalletError, Adapter } from "@solana/wallet-adapter-base";
 import { usePathname } from "next/navigation";
 import {
   LocaleProvider,
@@ -10,9 +11,12 @@ import {
   i18n,
 } from "@orderly.network/i18n";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
+import {
+  Network,
+  wagmiConnectors,
+  WalletConnectorPrivyProvider,
+} from "@orderly.network/wallet-connector-privy";
 import { useNav } from "@/hooks/useNav";
-import { PrivyWalletConnector } from "../PrivyWalletConnector";
-import { OrderlyPrivyAdapter } from "../OrderlyPrivyAdapter";
 import { useOrderlyConfig } from "@/hooks/useOrderlyConfig";
 import { usePathWithoutLang } from "@/hooks/usePathWithoutLang";
 
@@ -42,12 +46,56 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
     }
   }, [pathname]);
 
+  if (
+    !process.env.NEXT_PUBLIC_PRIVY_APP_ID ||
+    !process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
+  ) {
+    throw new Error("Privy App ID or Wallet Connect Project ID is not defined");
+  }
+
   return (
     <LocaleProvider
       onLanguageChanged={onLanguageChanged}
       backend={{ loadPath }}
     >
-      <PrivyWalletConnector>
+      <WalletConnectorPrivyProvider
+        termsOfUse="https://learn.woo.org/legal/terms-of-use"
+        network={Network.testnet}
+        privyConfig={{
+          appid: process.env.NEXT_PUBLIC_PRIVY_APP_ID!,
+          config: {
+            appearance: {
+              theme: "dark",
+              accentColor: "#181C23",
+            },
+          },
+        }}
+        wagmiConfig={{
+          connectors: [
+            wagmiConnectors.injected(),
+            wagmiConnectors.walletConnect({
+              projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
+              showQrModal: true,
+              storageOptions: {},
+              metadata: {
+                name: "Orderly Network",
+                description: "Orderly Network",
+                url: "https://orderly.network",
+                icons: ["https://oss.orderly.network/static/sdk/chains.png"],
+              },
+            }),
+          ],
+        }}
+        solanaConfig={{
+          mainnetRpc: "",
+          devnetRpc: "https://api.devnet.solana.com",
+          wallets: [],
+          onError: (error: WalletError, adapter?: Adapter) => {
+            console.log("-- error", error, adapter);
+          },
+        }}
+        abstractConfig={{}}
+      >
         <OrderlyAppProvider
           brokerId="orderly"
           brokerName="Orderly"
@@ -55,10 +103,9 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
           appIcons={config.orderlyAppProvider.appIcons}
           onRouteChange={onRouteChange}
         >
-          <OrderlyPrivyAdapter />
           {props.children}
         </OrderlyAppProvider>
-      </PrivyWalletConnector>
+      </WalletConnectorPrivyProvider>
     </LocaleProvider>
   );
 };
